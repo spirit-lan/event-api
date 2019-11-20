5ca1fb821a9c69b9cf2bdd7e1d8ca1c6eaa5e9cf
Julien <julien.vincent@alcuin.com>
2019-11-20 17:31:58 +0100

    remove role

--- src/service/role-service.ts
+++ src/service/role-service.ts
@@ -2,12 +2,20 @@ import { DbUser } from "../model/dto/db-user";
 import { User } from "../model/user";
 import { Role } from "../model/role";
 import { getRepository } from "typeorm";
-
+import * as _ from "lodash";
 
 export class RoleService {
     addRole(user: User, role: Role): Promise<DbUser> {
         if (user.roles.indexOf(role) == -1) {
             user.roles.push(role);
+            let repository = getRepository(DbUser);
+            return repository.save(user);
+        }
+    }
+
+    removeRole(user: User, role: Role): Promise<DbUser> {
+        if (user.roles.indexOf(role) == -1) {
+            _.remove(user.roles, (el: Role) => { return el.id === role.id });
             let repository = getRepository(DbUser);
             return repository.save(user);
         }
